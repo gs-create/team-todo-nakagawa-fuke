a82a1426c6ce803cd0412c0af3985b5f1ac5c6b0
FukeTakumi <t6cd24ac27b@gmail.com>
2020-03-20 17:44:15 +0900

    作成日と更新日の追加

--- db.sql
+++ db.sql
@@ -1,4 +1,4 @@
-mysql -uroot;
+mysql -uroot
 
 create database ToDoList;
 
@@ -7,5 +7,7 @@ use ToDoList;
 create table todos (id int AUTO_INCREMENT NOT NULL PRIMARY KEY, title varchar(255), text varchar(255));
 
 INSERT INTO todos (title,text) VALUE ("サンプル","ここに項目の説明を入力します");
+alter table todos add created_at timestamp default current_timestamp;
+alter table todos add updated_at timestamp default current_timestamp on update current_timestamp;
 
 exit
